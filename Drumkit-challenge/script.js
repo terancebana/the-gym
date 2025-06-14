6f4598977086ae63c0629c632613b2c0ebd5e9cf
BANA Terance <cyzterance06@gmail.com>
2025-06-14 15:29:31 +0200

    Added a simpler javascript

--- Drumkit-challenge/script.js
+++ Drumkit-challenge/script.js
@@ -1,23 +1,43 @@
 document.addEventListener('keydown', function(event) {
-    const key = event.key.toLowerCase();
-    const drumButtons = {
-        'w': { sound: 'sounds/crash.mp3', button: document.querySelector('button[data-key="65"]') },
-        'a': { sound: 'sounds/kick-bass.mp3', button: document.querySelector('button[data-key="83"]') },
-        's': { sound: 'sounds/snare.mp3', button: document.querySelector('button[data-key="68"]') },
-        'd': { sound: 'sounds/tom-1.mp3', button: document.querySelector('button[data-key="70"]') },
-        'j': { sound: 'sounds/tom-2.mp3', button: document.querySelector('button[data-key="74"]') },
-        'k': { sound: 'sounds/tom-3.mp3', button: document.querySelector('button[data-key="75"]') },
-        'l': { sound: 'sounds/tom-4.mp3', button: document.querySelector('button[data-key="76"]') }
-    };
+    const keyPressed = event.key.toLowerCase();
 
-    if (drumButtons[key]) {
-        const { sound, button } = drumButtons[key];
-        const audio = new Audio(sound);
-        audio.play();
-        
-        button.classList.add('playing');
-        setTimeout(() => {
-            button.classList.remove('playing');
-        }, 100);
+    switch (keyPressed) {
+        case 'w':
+            playDrum('sounds/crash.mp3', 'w-key');
+            break;
+        case 'a':
+            playDrum('sounds/kick-bass.mp3', 'a-key');
+            break;
+        case 's':
+            playDrum('sounds/snare.mp3', 's-key');
+            break;
+        case 'd':
+            playDrum('sounds/tom-1.mp3', 'd-key');13
+            break;
+        case 'j':
+            playDrum('sounds/tom-2.mp3', 'j-key');
+            break;
+        case 'k':
+            playDrum('sounds/tom-3.mp3', 'k-key');
+            break;
+        case 'l':
+            playDrum('sounds/tom-4.mp3', 'l-key');
+            break;
+        default:
+            break;
     }
 });
+
+function playDrum(soundPath, buttonDataKey) {
+    const audio = new Audio(soundPath);
+    audio.play();
+
+    const drumButton = document.querySelector(`button[data-key="${buttonDataKey}"]`);
+
+    if (drumButton) {
+        drumButton.classList.add('playing');
+        setTimeout(() => {
+            drumButton.classList.remove('playing');
+        }, 100);
+    }
+}
